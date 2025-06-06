268794fe2dc9134039c4d5a6fbbf5694231e57b6
Dante2332 <konrad.k.dabrowski@gmail.com>
2025-06-06 09:18:11 +0200

    Added Start Line

---
+++ Source/Praktyki/StartLine.cpp
@@ -0,0 +1,29 @@
+// Copyright 2025 Teyon. All Rights Reserved.
+
+
+#include "StartLine.h"
+
+// Sets default values
+AStartLine::AStartLine()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	LineMesh = CreateDefaultSubobject<UStaticMeshComponent>("LineMesh");
+	RootComponent = LineMesh;
+	
+}
+
+// Called when the game starts or when spawned
+void AStartLine::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AStartLine::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
